b4342a575d4c6e6c7693e50f9f9aa4b249ac02aa
unknown <aziretbp001@gmail.com>
2022-12-20 11:33:41 +0600

    first commit

---
+++ src/routes/MainRoutes.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AuthPage from "../pages/AuthPage";
+import HomePage from "../pages/HomePage";
+
+const MainRoutes = () => {
+  const PUBLIC_ROUTES = [
+    {
+      link: "/",
+      element: <HomePage />,
+      id: 1,
+    },
+    {
+      link: "/auth",
+      element: <AuthPage />,
+    },
+  ];
+  return (
+    <>
+      <Routes>
+        {PUBLIC_ROUTES.map((item) => (
+          <Route path={item.link} element={item.element} key={item.id} />
+        ))}
+      </Routes>
+    </>
+  );
+};
+
+export default MainRoutes;
