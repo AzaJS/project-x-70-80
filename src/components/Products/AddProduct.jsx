01a3629464ebdf3e12f059c8ac81a827fc860670
unknown <aziretbp001@gmail.com>
2022-12-22 10:14:49 +0600

    add started

--- src/components/Products/AddProduct.jsx
+++ src/components/Products/AddProduct.jsx
@@ -1,7 +1,70 @@
-import React from "react";
+import { Box, Button, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useProducts } from "../../contexts/productsContext";
 
 const AddProduct = () => {
-  return <div>add product</div>;
+  const navigate = useNavigate();
+  const { addProduct } = useProducts();
+
+  const [product, setProduct] = useState({
+    name: "",
+    description: "",
+    price: 0,
+    picture: "",
+    type: "",
+  });
+
+  return (
+    <Box sx={{ width: "60vw", margin: "10vh auto" }}>
+      <TextField
+        fullWidth
+        id="outlined-basic"
+        label="Name"
+        variant="outlined"
+        name="name"
+      />
+      <TextField
+        fullWidth
+        id="outlined-basic"
+        label="Description"
+        variant="outlined"
+        name="description"
+      />
+      <TextField
+        fullWidth
+        id="outlined-basic"
+        label="Price"
+        variant="outlined"
+        name="price"
+      />
+      <TextField
+        fullWidth
+        id="outlined-basic"
+        label="Picture"
+        variant="outlined"
+        name="picture"
+      />
+      <TextField
+        fullWidth
+        id="outlined-basic"
+        label="Type"
+        variant="outlined"
+        name="type"
+      />
+      <Button
+        onClick={() => {
+          addProduct(product);
+          navigate("/products");
+        }}
+        variant="outlined"
+        fullWidth
+        size="large"
+      >
+        CREATE PRODUCT
+      </Button>
+    </Box>
+  );
 };
 
 export default AddProduct;
